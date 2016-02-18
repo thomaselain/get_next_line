b765172264fb9411b16bc53e1e3d559049815f1b
Thomas ELAIN <telain@e1r9p6.42.fr>
2016-02-18 15:50:05 +0100

    ajout du main.c

--- main.c
+++ main.c
@@ -5,9 +5,23 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: telain <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/02/18 15:39:45 by telain            #+#    #+#             */
-/*   Updated: 2016/02/18 15:39:46 by telain           ###   ########.fr       */
+/*   Created: 2016/02/08 16:54:18 by telain            #+#    #+#             */
+/*   Updated: 2016/02/14 18:15:26 by telain           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+#include "get_next_line.h"
 
+int		main(void)
+{
+	char	**pline;
+	char	*line;
+	int		fd;
+
+	pline = &line;
+	fd = open("fichier", O_RDONLY, S_IREAD);
+	if (get_next_line(fd, pline) != -1)
+		ft_putstr(line);
+	return (0);
+}
